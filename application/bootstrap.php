84482a2150b6c5aa6b1f121c20b418d2b29f273b
Miodrag Tokić <mtokic@gmail.com>
2012-05-31 22:43:21 +0200

    Added missing setting comment to the bootstrap

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -55,6 +55,7 @@ Kohana::$environment = Kohana::DEVELOPMENT;
  * - string   index_file  name of your index file, usually "index.php"       index.php
  * - string   charset     internal character set used for input and output   utf-8
  * - string   cache_dir   set the internal cache directory                   APPPATH/cache
+ * - integer  cache_life  lifetime, in seconds, of items cached              60
  * - boolean  errors      enable or disable error handling                   TRUE
  * - boolean  profile     enable or disable internal profiling               TRUE
  * - boolean  caching     enable or disable internal caching                 FALSE
